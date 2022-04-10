3812621a1a36933af923f64be40a224b63b8157e
liutuo <616669577@qq.com>
2022-04-10 19:46:43 +0800

    demo

--- 0-dataset/mat2csv.py
+++ 0-dataset/mat2csv.py
@@ -7,9 +7,8 @@ print(mat.keys())
 
 for i in range(6):
     data = [mat[element[i]][:] for element in mat['X']]
-    res = np.transpose(data[0])
     np.savetxt('Handwritten_fea/X/X' + str(i) + '.txt',
-               res,
+               data[0],
                delimiter=" ",
                fmt="%.12f")
 
--- demo.cpp
+++ demo.cpp
@@ -6,6 +6,7 @@
 
 #include "mpi.h"
 using namespace std;
+using Eigen::MatrixXd;
 
 int main() {
     // dataset
@@ -15,11 +16,52 @@ int main() {
     string resPath = "./res-lmd0/";
     vector<string> metric = {"ACC", "nmi", "Purity", "Fscore", "Precision", "Recall", "AR", "Entropy"};
 
-    for (int dsi = 1; dsi < ds.size(); dsi++) {
+    for (int dsi = 0; dsi < ds.size(); dsi++) {
         vector<int> answer = {};
         // load data & make folder
         string dataName = ds[dsi];
         cout << dataName << endl;
         string path = dsPath + dataName;
+        vector<MatrixXd> X;
+
+        // load X
+        for (int t = 0; t < 6; t++) {
+            ifstream in(path + "/X/X" + to_string(t) + ".txt");
+            int row, col;
+            in >> row >> col;
+            MatrixXd Xi(row, col);
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    in >> Xi(i, j);
+                }
+            }
+            X.push_back(Xi);
+        }
+
+        // load Y
+        ifstream in(path + "/Y/Y.txt");
+        int row, col;
+        in >> row >> col;
+        MatrixXd Y(row, col);
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                in >> Y(i, j);
+            }
+        }
+
+        // there is no function 'unique' in cpp, so give the k directly
+        int k = 10;
+
+        // set the output path
+        // ...
+
+        // para setting
+        vector<int> d = {3 * k};  //[k 2*k 3*k]
+
+        for (int id = 0; id < d.size(); id++) {
+            // tic
+            // algo_AI
+            // toc
+        }
     }
 }
